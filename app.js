f307da7da762372b71fda087dea8ab1f7fe31b2a
Virgil Manalo <virgilio.manalo98@gmail.com>
2020-03-07 21:08:25 -0800

    Commit 3

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function getArray(){
     if (isNaN(start)==true){
         document.getElementById("array").innerHTML = "Invalid integer input, please reload and try again.";
     }
-}
 
 var end = prompt("Enter the end:");
 var endNum = parseInt(end);
@@ -27,4 +26,37 @@ var arrayNum = startNum;
     var binaryNum = startNum.toString(2);
 sumNum = startNum;
 genArray.push(startNum);
-binaryArray.push(binaryNum;)
+binaryArray.push(binaryNum);
+
+if (stepNum >= 0) {
+    for (i = startNum; i >= startNum && i< endNum;){
+        arrayNum = arrayNum + stepNum;
+        genArray.push(arrayNum);
+
+                    arrayBinary = (arrayNum).toString(2);
+                    binaryArray.push(arrayBinary);
+        sumNum = sumNum + arrayNum;
+        i = i + stepNum;
+    }
+}
+
+else if (stepNum <= 0) {
+    for (i = startNum; i <= startNum && i> endNum;) {
+        arrayNum = arrayNum +stepNum;
+        genArray.push(arrayNum);
+
+                    arrayBinary=(arrayNum).toString(2);
+                    binaryArray.push(arrayBinary);
+
+        sumNum = sumNum + arrayNum;
+        i = i + stepNum
+    }
+}
+    if (isNaN(start)==false && isNaN(end)==false && isNaN(step)==false){
+            document.getElementById("array").innerHTML = "The generated array is " + genArray;
+            document.getElementById("sum").innerHTML = "The generated sum is " + sumNum;
+            document.getElementById("binary").innerHTML = "The binary of absolute element values are: " + binaryArray;
+    }
+}
+
+getArray;
